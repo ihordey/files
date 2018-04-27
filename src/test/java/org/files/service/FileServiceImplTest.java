eca6b54a7b10e23c4691983dcaef52bd452f776f
Vanya <Ivan_Hordey@epam.com>
2018-04-27 10:18:09 +0300

    move test resource in corect folder

--- src/test/java/org/files/service/FileServiceImplTest.java
+++ src/test/java/org/files/service/FileServiceImplTest.java
@@ -12,7 +12,7 @@ public class FileServiceImplTest {
     @Test
     public void getDigest() throws Exception {
         String expectedDigest = "195c064cb99fd2cae41e16feaec3393733d742ee80a0ae43ee1ec91e9aae21dae4d5db1d6a7a3c0a54f05a78699a402bc612e0694925ed316610ed85a6c57dc4";
-        String absolutePath = new ClassPathResource("static/image.jpg").getFile().getAbsolutePath();
+        String absolutePath = new ClassPathResource("files/image.jpg").getFile().getAbsolutePath();
         assertThat(fileService.getDigest(absolutePath), is(expectedDigest));
     }
 }
